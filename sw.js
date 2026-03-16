9691a34a905aad98fe28f2e92fcbc23ef04f7ea7
gasdokumenter <gasdokumenter@gmail.com>
2026-03-16 17:40:02 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-const CACHE_NAME = 'gas-dok-v1';
-const assets = ['./index.html', './manifest.json'];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('fetch', function(event) {
+  // Biarkan request lewat, yang penting Service Worker terdeteksi aktif
 });
